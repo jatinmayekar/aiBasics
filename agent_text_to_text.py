5d8d6be17244dc4eaf90de1c6a522e16d8c619ce
Jatin Mayekar <44420409+jatinmayekar@users.noreply.github.com>
2023-11-11 17:16:05 -0500

    update agen_text_to_text.py
    add chat completions api

--- agent_text_to_text.py
+++ agent_text_to_text.py
@@ -17,20 +17,22 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     text_input = request.form['text_input']
-    # response = client.chat.completions.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[
-    #         {"role": "system", "content": "You are a helpful assistant."},
-    #         {"role": "user", "content": text_input}
-    #     ]
-    # )
-    response = client.completions.create(
-    model="gpt-3.5-turbo-instruct",
-    prompt=text_input
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": text_input}
+        ]
     )
-    print("here")
+    # response = client.completions.create(
+    # model="gpt-3.5-turbo-instruct",
+    # prompt=text_input
+    # )
+    
     print(response)
-    message_content = response.choices[0].text.strip() if response.choices else "No response"
+
+    # message_content = response.choices[0].text.strip() if response.choices else "No response"
+    message_content = response.choices[0].message.content if response.choices else "No response"
     
     return jsonify(message_content)
 
