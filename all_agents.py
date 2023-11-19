bdcd77756de8a09647b9869c678bdd3ae45ea99f
Jatin Mayekar <44420409+jatinmayekar@users.noreply.github.com>
2023-11-19 17:27:22 -0500

    combine text to function to all agents webapp

--- all_agents.py
+++ all_agents.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import openai
 import os
+import json
 
 load_dotenv()
 app = Flask(__name__)
@@ -12,24 +13,12 @@ openai.api_key = os.getenv('OPENAI_API_KEY')  # Replace with your actual OpenAI 
 client = OpenAI()
 debug=False
 
+def get_email_address(email_address):
+    return ("here is your latest email at " + email_address + ": " + "Hello, this is a test email.")
+
 @app.route('/')
 def index():
     return render_template('indexAll.html')
-
-@app.route('/text_to_text', methods=['POST'])
-def submit():
-    text_input = request.form['text_input']
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": text_input}
-        ]
-    )    
-    
-    if debug: print(response)
-    message_content = response.choices[0].message.content if response.choices else "No response"
-    return jsonify(message_content)
 
 @app.route('/text_to_speech', methods=['POST'])
 def text_to_speech():
@@ -43,8 +32,85 @@ def text_to_speech():
     )
     response.stream_to_file(speech_file_path)
 
+    if debug: print("Audio file path: " + speech_file_path)
+
     return send_file(speech_file_path, as_attachment=True)
 
+@app.route('/text_to_text_function', methods=['POST'])
+def submit():
+    text_input = request.form['text_input']
+    messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": text_input}
+    ]
+    tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_email_address",
+            "description": "Retrieve the most recent email from a specified email address. This function checks the inbox of the provided email address and returns the latest email, ensuring the user stays updated on their most recent correspondence.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "email_address": {
+                        "type": "string",
+                        "description": "The email address whose inbox will be checked for the latest email.",
+                    },
+                },
+                "required": ["email_address"],
+            },
+        },
+    }
+    ]
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=messages,
+        tools=tools,
+        tool_choice="auto",
+    )
+
+    if debug: print("Text to text & function output:" + response)
+    message_content = response.choices[0].message.content if response.choices else "No response"
+
+    response_msg = response.choices[0].message
+    tool_calls = response_msg.tool_calls
+    second_response = None
+
+     # Step 2: check if the model wanted to call a function
+    if tool_calls:
+        # Step 3: call the function
+        # Note: the JSON response may not always be valid; be sure to handle errors
+        available_functions = {
+            "get_email_address": get_email_address,
+        }  # only one function in this example, but you can have multiple
+        messages.append(response_msg)  # extend conversation with assistant's reply
+        # Step 4: send the info for each function call and function response to the model
+        for tool_call in tool_calls:
+            function_name = tool_call.function.name
+            function_to_call = available_functions[function_name]
+            function_args = json.loads(tool_call.function.arguments)
+            function_response = function_to_call(
+                email_address=function_args.get("email_address"),
+            )
+            messages.append(
+                {
+                    "tool_call_id": tool_call.id,
+                    "role": "tool",
+                    "name": function_name,
+                    "content": function_response,
+                }
+            )  # extend conversation with function response
+        second_response = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=messages,
+        )  # get a new response from the model where it can see the function response
+    print(second_response)
+    
+    if second_response == None:
+        second_response = response
+        
+    return jsonify(second_response.choices[0].message.content if response.choices else "No response")
 
 if __name__ == '__main__':
     app.run(debug=True)
