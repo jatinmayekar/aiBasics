92399fb205a1271126c346d797c467058f63d33b
Jatin Mayekar <44420409+jatinmayekar@users.noreply.github.com>
2023-12-03 19:01:35 -0500

    add static elements
    set elements as static to avoid reinitializing variables in a new session

--- test_streamlit.py
+++ test_streamlit.py
@@ -22,7 +22,8 @@ from load_dotenv import load_dotenv
 load_dotenv()
 
 openai.api_key = os.getenv("OPEN_API_KEY")
-client = OpenAI()
+if 'client' not in st.session_state:
+    st.session_state.client = OpenAI()
 
 st.title("OpenAI Bot")
 
@@ -38,31 +39,8 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    
-    file_name = uploaded_file.name
-    print(file_name)
-    
-    with open('swe_exhibitor_file.pdf', 'wb') as f:
-        f.write(bytes_data)
-    st.success("PDF file saved successfully.")
-
-    file_1 = client.files.create(
-        file=open("swe_exhibitor_file.pdf", "rb"),
-        purpose='assistants'
-    )
-    assistant = client.beta.assistants.create(
-        name="Exhibitor Assistant",
-        instructions="You are an greeter at the SWE 2023 conference. You have documents with information about the exhibitors."
-                    "Use your knowledgre retrieval skills to answer questions about the exhibitors.",
-        tools=[{"type": "code_interpreter"},{"type":"retrieval"}],
-        model="gpt-4-1106-preview",
-        file_ids=[file_1.id]
-    )
-elif uploaded_file is None:
-    assistant = client.beta.assistants.create(
+if 'assistant' not in st.session_state:
+    st.session_state.assistant = st.session_state.client.beta.assistants.create(
         name="Exhibitor Assistant",
         instructions="You are an greeter at the SWE 2023 conference. You have documents with information about the exhibitors."
                     "Use your knowledgre retrieval skills to answer questions about the exhibitors.",
@@ -70,7 +48,11 @@ elif uploaded_file is None:
         model="gpt-4-1106-preview"
     )
 
-thread = client.beta.threads.create()      
+if 'thread' not in st.session_state:
+    st.session_state.thread = st.session_state.client.beta.threads.create()      
+
+if "prev_uploaded_file" not in st.session_state: 
+    st.session_state.prev_uploaded_file = None
 
 prompt = st.chat_input("ask here...")
 if prompt=="exit":
@@ -82,29 +64,62 @@ if prompt:
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=prompt
-        )
+        if uploaded_file is not None:
+            # To read file as bytes:
+            bytes_data = uploaded_file.getvalue()
+            
+            file_name = uploaded_file.name
+            print(file_name)
 
-        run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant.id,
-        instructions="Please address the user as Jatin. The user has a premium account."
+            if st.session_state.prev_uploaded_file is not file_name:
+                print("File changed")
+                with open('swe_exhibitor_file.pdf', 'wb') as f:
+                    f.write(bytes_data)
+                st.success("PDF file saved successfully.")
+
+                file_1 = st.session_state.client.files.create(
+                    file=open("swe_exhibitor_file.pdf", "rb"),
+                    purpose='assistants'
+                )
+
+                message = st.session_state.client.beta.threads.messages.create(
+                    thread_id=st.session_state.thread.id,
+                    role="user",
+                    content=prompt,
+                    file_ids=[file_1.id]
+                )
+            else:
+                print("File not changed")
+                message = st.session_state.client.beta.threads.messages.create(
+                    thread_id=st.session_state.thread.id,
+                    role="user",
+                    content=prompt
+                )
+        elif uploaded_file is None:
+            print("No file uploaded")
+            message = st.session_state.client.beta.threads.messages.create(
+                thread_id=st.session_state.thread.id,
+                role="user",
+                content=prompt
+            )
+
+        run = st.session_state.client.beta.threads.runs.create(
+            thread_id=st.session_state.thread.id,
+            assistant_id=st.session_state.assistant.id,
+            instructions="Please address the user as Jatin. The user has a premium account."
         )
 
         while True:
             time.sleep(1)
-            run = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
+            run = st.session_state.client.beta.threads.runs.retrieve(
+                thread_id=st.session_state.thread.id,
                 run_id=run.id
             )
             if run.status == "completed":
                 break
 
-        messages = client.beta.threads.messages.list(
-        thread_id=thread.id
+        messages = st.session_state.client.beta.threads.messages.list(
+            thread_id=st.session_state.thread.id
         )
     st.success("Done!")
 
